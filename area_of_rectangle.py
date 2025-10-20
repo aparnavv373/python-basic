e26d68de9cb968d0e35567b976b175b3a4a54dfc
aparna <aparnavv631@gmail.com>
2025-10-20 11:17:02 +0530

    Initial commit with README

---
+++ area_of_rectangle.py
@@ -0,0 +1,13 @@
+length=int(input("Enter length:"))
+breadth=int(input("Enter breadth:"))
+area=length*breadth
+print("Area of rectangle:",area)
+
+
+
+
+
+
+
+
+
---
+++ average_of_three_numbers.py
@@ -0,0 +1,6 @@
+first_number=int(input("Enter first number:"))
+second_number=int(input("Enter second number:"))
+third_number=int(input("Enter Third number:"))
+# average of three numbers
+avg=(first_number+second_number+third_number)/3
+print("Average:",avg)
